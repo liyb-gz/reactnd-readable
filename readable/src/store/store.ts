c49f55fcf215b78cdecf670e4daa85298b11940e
Felix Li <felix@FelixPro.local>
2020-07-22 00:35:51 +0800

    delete the counter component that comes with starter

--- readable/src/store/store.ts
+++ readable/src/store/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/Counter/counterSlice';
 import uiReducer from './uiSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     ui: uiReducer,
   },
 });
