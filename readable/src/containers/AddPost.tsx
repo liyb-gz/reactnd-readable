5522d410f7325b976765695df5e698c243e5c69f
Felix Li <lcfelixli@ust.hk>
2020-07-21 20:37:41 +0800

    Add components for new post

--- readable/src/containers/AddPost.tsx
+++ readable/src/containers/AddPost.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import React from "react";
+import {
+  TextField,
+  Grid,
+  makeStyles,
+  Theme,
+  createStyles,
+  Button,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    post: {
+      "& $formControl": {
+        marginTop: theme.spacing(2),
+      },
+    },
+    formControl: {},
+  })
+);
 
 interface Props {}
 
 const AddPost = (props: Props) => {
-  return <div>Add Post</div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>Add Post</h1>
+      <Grid container>
+        <Grid item xs={12}>
+          <form className={classes.post}>
+            <TextField
+              label="Title"
+              required
+              fullWidth
+              variant="outlined"
+              className={classes.formControl}
+            />
+            <TextField
+              label="Post"
+              required
+              placeholder="Write something..."
+              multiline
+              rows={20}
+              fullWidth
+              variant="outlined"
+              className={classes.formControl}
+            />
+            <Button
+              color="primary"
+              variant="contained"
+              className={classes.formControl}
+            >
+              Submit
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default AddPost;
